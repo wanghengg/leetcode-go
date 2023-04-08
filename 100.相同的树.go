e3e1bd53f785222577f7ede7f9be6ac26d5eb0ed
wangheng <wh9697yyk@163.com>
2023-04-08 15:55:32 +0800

    相同的树-递归分解

---
+++ 100.相同的树.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=golang
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package leetcodego
+
+// 判断两个树是否完全相等，可以先判断当前节点是否相等，然后其左右子树是否分别相等，只要有一个不
+// 相等就返回false，全部都相等返回true
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
+// @lc code=end
