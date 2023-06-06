3d67f6dcc0c3711d3808ee7e4f3dd2867735ba69
hengwang <wh9697yyk@163.com>
2023-06-06 11:24:25 +0800

    反转字符串中的单词-先整体反转然后再单词反转

--- 151.反转字符串中的单词.go
+++ 151.反转字符串中的单词.go
@@ -28,8 +28,8 @@ func reverseWords(s string) string {
 	}
 	arr := strings.Split(string(str), " ")
 	var i, j int
-	j = len(arr)-1
-	for ; i < j; {
+	j = len(arr) - 1
+	for i < j {
 		tmp := arr[i]
 		arr[i] = arr[j]
 		arr[j] = tmp
@@ -37,6 +37,58 @@ func reverseWords(s string) string {
 		j--
 	}
 	return strings.Join(arr, " ")
+}
+
+/*
+ * 现将整个字符串反转，然后再将每个单词反转
+ */
+func reverseWords1(s string) string {
+	// 去除字符串中的多余空格
+	arrStr := []byte(s)
+	first, second := 0, 0
+	for second < len(arrStr) {
+		for second < len(arrStr) && arrStr[second] == ' ' {
+			second++
+		}
+		if second > 0 && second < len(arrStr) && arrStr[second-1] == ' ' {
+			if first != 0 {
+				arrStr[first] = ' '
+				first++
+			}
+		}
+		if second >= len(arrStr) {
+			break
+		}
+		arrStr[first] = arrStr[second]
+		first++
+		second++
+	}
+	arrStr = arrStr[0:first]
+	// 将整个字符串反转
+	for i, j := 0, len(arrStr)-1; i < j; i, j = i+1, j-1 {
+		arrStr[i], arrStr[j] = arrStr[j], arrStr[i]
+	}
+	// 将每个单词反转
+	first, second = 0, 0
+	for ind := 0; ind < len(arrStr); ind++ {
+		if arrStr[ind] != ' ' {
+			continue
+		}
+		second = ind - 1
+		for first < second {
+			arrStr[first], arrStr[second] = arrStr[second], arrStr[first]
+			first++
+			second--
+		}
+		first = ind + 1
+	}
+	second = len(arrStr)-1
+	for first < second {
+		arrStr[first], arrStr[second] = arrStr[second], arrStr[first]
+		first++
+		second--
+	}
+	return string(arrStr)
 }
 
 // @lc code=end
