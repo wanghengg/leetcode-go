008d71e2be023fc59eaf3e072e10f1b7d5d27b18
hengwang <wh9697yyk@163.com>
2023-06-06 14:09:20 +0800

    对称翻转

---
+++ 48.旋转图像.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=golang
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+package leetcodego
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			matrix[i][j], matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1] =
+				matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1], matrix[i][j]
+		}
+	}
+}
+
+func rotate1(matrix [][]int)  {
+	// 上下翻转
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < n; j++ {
+			matrix[i][j], matrix[n-i-1][j] = matrix[n-i-1][j], matrix[i][j]
+		}
+	} 
+	// 沿主对角线翻转
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
+
+// @lc code=end
