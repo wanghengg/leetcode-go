c19ce0424764d5d41ad4bb80fda84cc86d76b501
hengwang <wh9697yyk@163.com>
2023-06-14 23:32:35 +0800

    动态规划

---
+++ 32.最长有效括号.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=golang
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+package leetcodego
+
+func longestValidParentheses(s string) int {
+	res := 0
+	stack := []int{}
+	stack = append(stack, -1)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1] // 弹出一个匹配的左括号
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				res = max(res, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return res
+}
+
+func longestValidParentheses1(s string) int {
+	res := 0
+	dp := make([]int, len(s))
+	for i := 1; i < len(s); i++ {
+		if s[i] == ')' {
+			if s[i-1] == '(' {
+				if i >= 2 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
+				if i-dp[i-1] >= 2 {
+					dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
+				} else {
+					dp[i] = dp[i-1] + 2
+				}
+			}
+			res = max(res, dp[i])
+		}
+	}
+	return res
+}
+
+// @lc code=end
