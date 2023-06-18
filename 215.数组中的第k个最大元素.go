c08df50202d3fb03e3e87689dfb9b91fc8808f01
hengwang <wh9697yyk@163.com>
2023-06-18 22:38:23 +0800

    快速排序&堆排序

--- 215.数组中的第k个最大元素.go
+++ 215.数组中的第k个最大元素.go
@@ -35,4 +35,35 @@ func findKthLargest(nums []int, k int) int {
 	return partition(nums, 0, len(nums)-1, len(nums)-k)
 }
 
+func findKthLargest1(nums []int, k int) int {
+	heapSize := len(nums)
+	buildMaxHeap(nums, heapSize)
+	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		heapSize--
+		maxHeapify(nums, 0, heapSize)
+	}
+	return nums[0]
+}
+
+func buildMaxHeap(nums []int, heapSize int) {
+	for i := heapSize / 2; i >= 0; i-- {
+		maxHeapify(nums, i, heapSize)
+	}
+}
+
+func maxHeapify(nums []int, i, heapSize int) {
+	l, r, largest := 2*i+1, 2*i+2, i
+	if l < heapSize && nums[l] > nums[largest] {
+		largest = l
+	}
+	if r < heapSize && nums[r] > nums[largest] {
+		largest = r
+	}
+	if largest != i {
+		nums[i], nums[largest] = nums[largest], nums[i]
+		maxHeapify(nums, largest, heapSize)
+	}
+}
+
 // @lc code=end
