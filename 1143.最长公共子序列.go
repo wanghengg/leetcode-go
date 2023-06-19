8d1d17c6e6edbd013fa7a38d30517290b1a5daf5
hengwang <wh9697yyk@163.com>
2023-06-19 13:38:30 +0800

    动态规划

---
+++ 1143.最长公共子序列.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=golang
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+package leetcodego
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	n1, n2 := len(text1), len(text2)
+	dp := make([][]int, n1+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, n2+1)
+	}
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[n1][n2]
+}
+
+// @lc code=end
