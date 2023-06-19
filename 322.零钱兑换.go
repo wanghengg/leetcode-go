2c94bae8582b891cfc37876b3b469a7fdeb05882
hengwang <wh9697yyk@163.com>
2023-06-19 15:03:36 +0800

    动态规划

---
+++ 322.零钱兑换.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=golang
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+package leetcodego
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 0; i <= amount; i++ {
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i >= coin {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	} else {
+		return dp[amount]
+	}
+}
+
+// @lc code=end
