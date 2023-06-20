dfe24f7dc958697e544cb815bd96868a67dae656
hengwang <wh9697yyk@163.com>
2023-06-20 14:17:36 +0800

    style

--- 17.电话号码的字母组合.go
+++ 17.电话号码的字母组合.go
@@ -12,14 +12,14 @@ func letterCombinations(digits string) []string {
 		return []string{}
 	}
 	m := map[rune][]rune{
-		'2': []rune{'a', 'b', 'c'},
-		'3': []rune{'d', 'e', 'f'},
-		'4': []rune{'g', 'h', 'i'},
-		'5': []rune{'j', 'k', 'l'},
-		'6': []rune{'m', 'n', 'o'},
-		'7': []rune{'p', 'q', 'r', 's'},
-		'8': []rune{'t', 'u', 'v'},
-		'9': []rune{'w', 'x', 'y', 'z'},
+		'2': {'a', 'b', 'c'},
+		'3': {'d', 'e', 'f'},
+		'4': {'g', 'h', 'i'},
+		'5': {'j', 'k', 'l'},
+		'6': {'m', 'n', 'o'},
+		'7': {'p', 'q', 'r', 's'},
+		'8': {'t', 'u', 'v'},
+		'9': {'w', 'x', 'y', 'z'},
 	}
 	res := []string{}
 	var dfs func(digits string, ind int, combine string)
