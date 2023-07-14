f92daf225b7bb4eaa8428d1694ef3604912c5527
hengwang <wh9697yyk@163.com>
2023-07-14 10:42:26 +0800

    二叉树的最近公共祖先-递归

---
+++ 236.二叉树的最近公共祖先.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=golang
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package leetcodego
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
+
+// @lc code=end
